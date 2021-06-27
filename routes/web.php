7e4e7e4262b27c667dd870b63f76b7394530b03a
hds39 <hds39@outlook.jp>
2021-06-27 16:02:33 +0900

    商品購入画面の作成

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('', 'ItemsController@showItems')->name('top');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 Route::get('items/{item}', 'ItemsController@showItemDetail')->name('item');
 
 Route::middleware('auth')
@@ -35,4 +34,5 @@ Route::prefix('mypage')
          Route::get('edit-profile', 'ProfileController@showProfileEditForm')->name('mypage.edit-profile');
          Route::post('edit-profile', 'ProfileController@editProfile')->name('mypage.edit-profile');
          Route::get('sold-items', 'SoldItemsController@showSoldItems')->name('mypage.sold-items');
+         Route::get('bought-items', 'BoughtItemsController@showBoughtItems')->name('mypage.bought-items');
      });
